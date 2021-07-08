47d4c40fe9980fc78c959db45b289edcfcf62d47
Theo Beers <theo.beers@fu-berlin.de>
2021-07-08 11:02:49 +0200

    Update dependency

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
-#! /opt/homebrew/bin/bash
+#! /usr/bin/env bash
 
-npx sort-package-json &&
-npm install &&
+set -Eeuo pipefail
 
-npm run prettify-ts &&
-npm run standardize-ts &&
-
-npm run compile &&
-mv ts/dozenal.js js/dozenal.js &&
-
-npm run prettify-js &&
-npm run standardize-js &&
-
-npm run prettify-html
+sort-package-json &&
+	npm install &&
+	npm run prettify-ts &&
+	npm run standardize-ts &&
+	npm run compile &&
+	mv ts/dozenal.js js/dozenal.js &&
+	npm run prettify-js &&
+	npm run standardize-js &&
+	npm run prettify-html
